2bbe1e1eabaef924c4240c9335398023faa40ff2
Nishith Munasinghe <luxecat3@gmail.com>
2024-11-29 19:46:42 +1100

    sigma-ify sketchybars

--- .config/sketchybar/colors.lua
+++ .config/sketchybar/colors.lua
@@ -1,25 +1,25 @@
 return {
-  black = 0xff181819,
-  white = 0xffe2e2e3,
-  red = 0xfffc5d7c,
-  green = 0xff9ed072,
-  blue = 0xff76cce0,
-  yellow = 0xffe7c664,
-  orange = 0xfff39660,
-  magenta = 0xffb39df3,
-  grey = 0xff7f8490,
+  black = 0xff282c34,
+  white = 0xffabb2bf,
+  red = 0xffe06c75,
+  green = 0xff98c379,
+  blue = 0xff61afef,
+  yellow = 0xffe5c07b,
+  orange = 0xffe5c07b,
+  magenta = 0xffe06c75,
+  grey = 0xff282c34,
   transparent = 0x00000000,
 
   bar = {
-    bg = 0xf02c2e34,
-    border = 0xff2c2e34,
+    bg = 0xff282c34,
+    border = 0xffabb2bf,
   },
   popup = {
-    bg = 0xc02c2e34,
-    border = 0xff7f8490
+    bg = 0xff282c34,
+    border = 0xffabb2bf
   },
-  bg1 = 0xff363944,
-  bg2 = 0xff414550,
+  bg1 = 0xff282c34,
+  bg2 = 0xff282c34,
 
   with_alpha = function(color, alpha)
     if alpha > 1.0 or alpha < 0.0 then return color end
